6ff45d411c31331b1d0f151a30c35031580b2a74
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-02-14 18:16:44 -0300

    - Added function to reset  the counter display when starting the page.

--- Practica-WebForms/WebFormContador.aspx
+++ Practica-WebForms/WebFormContador.aspx
@@ -9,6 +9,9 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:Label runat="server" ID="LblContador" Enabled="false"/>
+            <br/>
+            <asp:LinkButton runat="server" ID="BtnIncrementar">Guardar en base de datos e incrementar</asp:LinkButton>
         </div>
     </form>
 </body>
--- Practica-WebForms/WebFormContador.aspx.cs
+++ Practica-WebForms/WebFormContador.aspx.cs
@@ -4,13 +4,57 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Practica_WebForms.DAL;
 
 namespace Practica_WebForms
 {
     public partial class WebFormContador : System.Web.UI.Page
     {
+        //Property
+        private const int CONTADOR_INICIAL = 100000;
+        public static int ContadorInicial { get { return CONTADOR_INICIAL; } }
+
+        //Entity Framework
+        ContadorEntities Contadores = new ContadorEntities();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            if (!this.IsPostBack)
+            {
+
+                //Eliminar todos los registros y settear el contador en su valor CONTADOR_INICIAL
+                this.EliminarTodo();
+                LblContador.Text = ContadorInicial.ToString();
+
+            }
+            else
+            {
+
+
+
+            }
+        }
+
+        protected void EliminarTodo()
+        {
+            int longitud = Contadores.Numeros.ToList().Count;
+
+            try
+            {
+                for (int i = 0; i < longitud; i++)
+                {
+                    Numero num = Contadores.Numeros.Find(Contadores.Numeros.ToList()[i].Id);
+                    Contadores.Numeros.Remove(num);
+                }
+
+                Contadores.SaveChanges();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se pudieron eliminar los registros");
+
+            }
 
         }
     }
