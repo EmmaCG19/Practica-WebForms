c64724cd7f2201fe3eb6a12db4eb49b816705ef8
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-02-18 16:54:58 -0300

    Added SPA project, demonstrating the use of Panels.

---
+++ Practica-WebForms/WebFormSPA.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormSPA.aspx.cs" Inherits="Practica_WebForms.WebFormSPA" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>SPA: Single-Page Application</h1>
+            <asp:Panel runat="server" ID="PanelLogin">
+                <asp:Label runat="server" ID="LblClave" Text="Clave:" />
+                <asp:TextBox runat="server" ID="TxtBoxClave" />
+                <asp:Button runat="server" ID="BtnLogin" Text="Iniciar Sesion" OnClick="BtnLogin_Click" />
+                <br />
+                <asp:Label runat="server" ID="LblLoginValido"></asp:Label>
+            </asp:Panel>
+            <asp:Panel runat="server" ID="PanelBase64">
+                <asp:LinkButton runat="server" ID="LinkBtnCerrar" Text="Cerrar Sesión" OnClick="LinkBtnCerrar_Click" />
+                <br />
+                <table>
+                    <tr>
+                        <td><asp:Label runat="server" ID="LblTextSinCodif" Text="Texto Sin Codificar." /></td>
+                        <td></td>
+                        <td><asp:Label runat="server" ID="LblTextCodif" Text="Texto Codificado." /></td>
+                    </tr>
+                    <tr>
+                        <td><asp:TextBox runat="server" ID="TxtBoxSinCodif"/></td>
+                        <td><asp:Button runat="server" ID="BtnCodificar" Text="Codificar a Base64" OnClick="BtnCodificar_Click" /></td>
+                        <td><asp:TextBox runat="server" ID="TxtBoxCodif"/></td>
+                    </tr>
+                </table>
+                
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practica-WebForms/WebFormSPA.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Practica_WebForms.Helpers;
+
+namespace Practica_WebForms
+{
+    public partial class WebFormSPA : System.Web.UI.Page
+    {
+        private const string PASSWORD = "admin";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.ShowLogin();
+            }
+        }
+
+        protected void ShowLogin()
+        {
+            PanelLogin.Visible = true;
+            PanelBase64.Visible = !PanelLogin.Visible;
+        }
+
+        protected void ShowBase64()
+        {
+            PanelBase64.Visible = true;
+            PanelLogin.Visible = !PanelBase64.Visible;
+        }
+
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (TxtBoxClave.Text.Equals(PASSWORD))
+            {
+                LblLoginValido.Text = "Bienvenido!";
+                this.ShowBase64();
+            }
+            else
+            {
+                LblLoginValido.ForeColor = System.Drawing.Color.Red;
+                LblLoginValido.Text = "La clave no es válida. Vuelva a intentarlo.";
+            }
+        }
+
+        protected void BtnCodificar_Click(object sender, EventArgs e)
+        {
+            TxtBoxCodif.Text = Base64.Encode(TxtBoxSinCodif.Text); 
+
+        }
+
+        protected void LinkBtnCerrar_Click(object sender, EventArgs e)
+        {
+            this.CleanAll();
+            this.ShowLogin();
+        }
+
+        private void CleanAll()
+        {
+            LblLoginValido.Text = TxtBoxClave.Text = TxtBoxCodif.Text = TxtBoxSinCodif.Text = String.Empty;
+        }
+    }
+}
