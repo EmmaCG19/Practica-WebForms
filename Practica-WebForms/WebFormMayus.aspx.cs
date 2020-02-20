07eef23b8bc9ddc7f3d1aeab33c0fede37d3ba63
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-02-20 16:13:53 -0300

    Revert "Revert "Added WebFormMayus""
    This reverts commit b61c2ca5444c7a6ceae7a6504fc5633b1e3e13b3.

---
+++ Practica-WebForms/WebFormMayus.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Practica_WebForms.DAL;
+
+namespace Practica_WebForms
+{
+    public partial class WebFormMayus : System.Web.UI.Page
+    {
+        EscuelaDBEntities context = new EscuelaDBEntities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            this.MostrarEstudiantes();
+
+        }
+
+        protected void MostrarEstudiantes()
+        {
+            GWEstudiantes.DataSource = context.Estudiantes.ToList();
+            GWEstudiantes.DataBind();
+        }
+
+        protected void BtnConvertir_Click(object sender, EventArgs e)
+        {
+            this.ConvertirAMayus(); 
+
+        }
+
+        //Modificar a aquellos alumnos que tengan un DNI  == Cantidad Adeudada
+        protected void ConvertirAMayus()
+        {
+            List<Estudiante> estudiantesMismoDNIDeuda = context.Estudiantes.Where(e => e.NroDocumento.Equals(e.CantidadAdeudada)).ToList();
+
+            foreach (Estudiante est in estudiantesMismoDNIDeuda)
+            {
+                est.Sobrenombre = est.Sobrenombre.ToUpper();
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
